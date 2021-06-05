61cd9c25be6aa1bbe1abf014d365631b75f8c526
Harrison <>
2021-06-05 10:10:01 +0000

    Added more features, fixed some scrolling bugs, edited js to remove some redundant scripts

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Smooth scrolling function
+// Code from: 
+
+
 $(document).ready(function () {
   var timeStart = 0;
 
@@ -63,80 +67,8 @@ $(document).ready(function () {
     }
   }
 
-  function dragStart(e) {
-    e.preventDefault();
-    if (e.type == "touchstart") {
-      $(document).off("mousedown", dragStart);
-      startPoint = e.originalEvent.touches[0].pageY;
-    } else {
-      startPoint = e.pageY;
-    }
-    dragDist = 0;
-    $(document).on("touchmove mousemove", dragMove);
-  }
-
-  function dragMove(e) {
-    if (e.type == "touchmove") {
-      var movePoint = e.originalEvent.touches[0].pageY;
-    } else {
-      var movePoint = e.pageY;
-    }
-    dragDist = ((movePoint - startPoint) / $(".slider").height()) * 100;
-
-    $(".slider").addClass("dragging");
-
-    $(".slide, .bg").css({
-      transition: "0ms",
-    });
-
-    if (dragDist < 0) {
-      $(".active .bg").css({
-        opacity: 1 + dragDist / 200,
-      });
-      $(".active")
-        .css({
-          transform: "translate3d(0," + dragDist / 2 + "%,0)",
-        })
-        .next()
-        .css({
-          transform: "translate3d(0," + (100 + dragDist) + "%,0)",
-        });
-    }
-
-    if (dragDist > 0) {
-      $(".active")
-        .css({
-          transform: "translate3d(0," + dragDist + "%,0)",
-        })
-        .prev()
-        .css({
-          animation: "none",
-          transform: "translate3d(0," + (-50 + dragDist / 2) + "%,0)",
-        })
-        .find(".bg")
-        .css({
-          opacity: 0.5 + dragDist / 200,
-        });
-    }
-  }
-
-  function dragEnd() {
-    $(document).off("touchmove mousemove", dragMove);
-    $(".slide, .bg").removeAttr("style");
-    if (dragDist > 20) {
-      prevSlide();
-    }
-    if (dragDist < -20) {
-      nextSlide();
-    }
-
-    setTimeout(function () {
-      $(".slider").removeClass("dragging");
-    }, 800);
-  }
-
   $(document).on("wheel", wheely);
   $(document).on("keydown", hotkeys);
-  $(document).on("touchstart mousedown", dragStart);
-  $(document).on("touchend mouseup", dragEnd);
+ // $(document).on("touchstart mousedown", dragStart);
+ // $(document).on("touchend mouseup", dragEnd);
 });
